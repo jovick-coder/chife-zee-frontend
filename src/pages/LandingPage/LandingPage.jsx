09d60fad4ea2486f931b4442969f5c0fdb40ca89
Jovick-coder <victorjosiahm3@gmail.com>
2022-08-15 13:58:47 +0100

    landing page

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroComponent from "../../components/Hero/HeroComponent";
+
+function LandingPage() {
+  return (
+    <div>
+      <HeroComponent />
+      LandingPage
+    </div>
+  );
+}
+
+export default LandingPage;
